ebe79e8abd1816998a38bac39caca43e360166fe
Dhruv Talati <dhruvtalati640@gmail.com>
2026-09-17 12:37:14 +0530

    Updatedd

--- ReactJS-Spring-Boot-CRUD-Full-Stack-App/springboot-backend/src/main/java/net/javaguides/springboot/config/CorsConfig.java
+++ ReactJS-Spring-Boot-CRUD-Full-Stack-App/springboot-backend/src/main/java/net/javaguides/springboot/config/CorsConfig.java
@@ -18,7 +18,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 public class CorsConfig {
 
-    @Value("${app.cors.allowed-origin}")
+    @Value("${app.cors.allowed-origin:http://localhost:3000}")
     private String allowedOrigin;
 
     @Bean
